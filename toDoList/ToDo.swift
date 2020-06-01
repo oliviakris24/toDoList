f1b6f5e3be38480f0223dc192e551278bd3f597e
Olivia Kris <oliviakris@Olivias-Air-2.frontierlocal.net>
2020-05-31 19:04:19 -0700

    hi

---
+++ toDoList/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Olivia Kris on 5/31/20.
+//  Copyright © 2020 Olivia Kris. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
+
+

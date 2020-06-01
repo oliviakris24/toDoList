169cbb0fd2155585c9141ffbeae7521759fb3dba
Olivia Kris <oliviakris@Olivias-Air-2.frontierlocal.net>
2020-05-31 21:22:36 -0700

    complete iteration 0 and some of iteration 1

--- toDoList/TableViewController.swift
+++ toDoList/TableViewController.swift
@@ -92,15 +92,18 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let addVC = segue.destination as? AddToDoViewController {
+            addVC.previousVC = self
+        }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 
 }
